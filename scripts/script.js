bfd8c07e2c7cd80a8309cced7c89246ec1ea944a
Kyryl Shtanhei <kyryl@shtanhei.org>
2024-03-17 02:43:49 -0700

    Begin working on map view

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,15 @@
-function sayHello() {
-    
+//---------------------------------
+// Your own functions here
+//---------------------------------
+
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      }).catch((error) => {
+        // An error happened.
+      });
 }
-//sayHello();
