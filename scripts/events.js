693774702e64e3d6483a91b53c2c00c75d84e835
persephone-wong <persephone.w315@gmail.com>
2024-03-18 01:37:41 -0700

    delete photos and changed events.js code

--- scripts/events.js
+++ scripts/events.js
@@ -3,26 +3,26 @@ function displayEventCards(collection) {
 
     db.collection(collection).get()   //the collection called "hikes"
         .then(allEvents=> {
-            var i = 1;  //Optional: if you want to have a unique ID for each event
+            // var i = 1;  //Optional: if you want to have a unique ID for each event
             allEvents.forEach(doc => { //iterate thru each doc
                 var title = doc.data().name;
                 var address = doc.data().address;
                 var time = doc.data().time;       
-                var description = doc.data().details;  // get value of the "details" key
-								var hikeCode = doc.data().code;    
+                var description = doc.data().description;  // get value of the "details" key
+								var eventcode = doc.data().code;    
                 let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
 
                 //update title and text and image
                 newcard.querySelector('.card-title').innerHTML = title;
-                newcard.querySelector('.card-address').innerHTML = address;
+                newcard.querySelector('.card-location').innerHTML = address;
                 newcard.querySelector('.card-time').innerHTML = time;
                 newcard.querySelector('.card-description').innerHTML = description;
-                newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+                newcard.querySelector('.card-img').src = `./images/${eventcode}.jpg`;
 
-                //unique ids to all elements for future use
-                newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
-                newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
-                newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+                // //unique ids to all elements for future use
+                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
 
                 //attach to gallery, Example: "hikes-go-here"
                 document.getElementById(collection + "-go-here").appendChild(newcard);
