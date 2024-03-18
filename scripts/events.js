256d35167eaceb097843605a54eda3b152ba2d30
persephone-wong <persephone.w315@gmail.com>
2024-03-18 01:09:07 -0700

    Add scripts to list page

--- scripts/events.js
+++ scripts/events.js
@@ -2,25 +2,27 @@ function displayEventCards(collection) {
     let cardTemplate = document.getElementById("eventCardTemplate");
 
     db.collection(collection).get()   //the collection called "hikes"
-        .then(allHikes=> {
-            //var i = 1;  //Optional: if you want to have a unique ID for each hike
-            allHikes.forEach(doc => { //iterate thru each doc
-                var title = doc.data().name;       // get value of the "name" key
-                var details = doc.data().details;  // get value of the "details" key
-								var hikeCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
-                var hikeLength = doc.data().length; //gets the length field
+        .then(allEvents=> {
+            var i = 1;  //Optional: if you want to have a unique ID for each event
+            allEvents.forEach(doc => { //iterate thru each doc
+                var title = doc.data().name;
+                var address = doc.data().address;
+                var time = doc.data().time;       
+                var description = doc.data().details;  // get value of the "details" key
+								var hikeCode = doc.data().code;    
                 let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
 
                 //update title and text and image
                 newcard.querySelector('.card-title').innerHTML = title;
-                newcard.querySelector('.card-length').innerHTML = hikeLength +"km";
-                newcard.querySelector('.card-text').innerHTML = details;
+                newcard.querySelector('.card-address').innerHTML = address;
+                newcard.querySelector('.card-time').innerHTML = time;
+                newcard.querySelector('.card-description').innerHTML = description;
                 newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
 
-                //Optional: give unique ids to all elements for future use
-                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
-                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
-                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+                //unique ids to all elements for future use
+                newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
 
                 //attach to gallery, Example: "hikes-go-here"
                 document.getElementById(collection + "-go-here").appendChild(newcard);
@@ -30,4 +32,4 @@ function displayEventCards(collection) {
         })
 }
 
-displayCardsDynamically("hikes"); 
+displayEventCards("events"); 
