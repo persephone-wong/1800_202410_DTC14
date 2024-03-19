80be786dacb0a208a914cdde0e9831bc04ba938a
Kyryl Shtanhei <kyryl@shtanhei.org>
2024-03-18 21:56:24 -0700

    Update event.html to use template

--- scripts/eachEvent.js
+++ scripts/eachEvent.js
@@ -2,6 +2,10 @@ function displayEventInfo() {
     let params = new URLSearchParams(window.location.search);
     let ID = params.get("id"); // Get value for key "id"
     console.log(ID);
+    
+    let cardTemplate = document.getElementById("eventCardTemplate");
+    let card = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+
   
     db.collection("events")
       .doc(ID)
@@ -17,15 +21,16 @@ function displayEventInfo() {
           var eventCode = data.code;
           var waitTime = data.typical_wait_time;
   
-          document.querySelector(".eventName").innerHTML = name;
-          document.querySelector(".eventLocation").innerHTML = location;
-          document.querySelector(".eventTime").innerHTML = time;
-          document.querySelector(".eventDate").innerHTML = date;
-          document.querySelector(".eventDetails").innerHTML = description;
-          document.querySelector(".historicTimeWait").innerHTML = waitTime;
+          card.querySelector(".eventName").innerHTML = name;
+          card.querySelector(".eventLocation").innerHTML = location;
+          card.querySelector(".eventTime").innerHTML = time;
+          card.querySelector(".eventDate").innerHTML = date;
+          card.querySelector(".eventDetails").innerHTML = description;
+          card.querySelector(".historicTimeWait").innerHTML = waitTime;
   
-          let imgEvent = document.querySelector(".eventImage");
+          let imgEvent = card.querySelector(".eventImage");
           imgEvent.src = "../images/" + eventCode + ".jpg";
+          document.getElementById("eventContainer").appendChild(card);
         } else {
           console.log("No such document!");
         }
