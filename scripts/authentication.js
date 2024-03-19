7aef05e834e5acf7df63cee6ec541e3b7b61827e
Kyryl Shtanhei <kyryl@shtanhei.org>
2024-03-18 22:39:51 -0700

    Change authentication.js to create a new doc in firestore when new user creates account

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -7,7 +7,47 @@ var uiConfig = {
       // User successfully signed in.
       // Return type determines whether we continue the redirect automatically
       // or whether we leave that to developer to handle.
-      return true;
+      //------------------------------------------------------------------------------------------
+      // The code below is modified from default snippet provided by the FB documentation.
+      //
+      // If the user is a "brand new" user, then create a new "user" in your own database.
+      // Assign this user with the name and email provided.
+      // Before this works, you must enable "Firestore" from the firebase console.
+      // The Firestore rules must allow the user to write.
+      //------------------------------------------------------------------------------------------
+      var user = authResult.user; // get the user object from the Firebase authentication database
+      if (authResult.additionalUserInfo.isNewUser) {
+        //if new user
+        navigator.geolocation.getCurrentPosition(position => {
+          const userLocation = [position.coords.longitude, position.coords.latitude];
+          // Perform Firestore set operation inside geolocation callback
+          db.collection("users").doc(user.uid).set({
+            name: user.displayName,
+            email: user.email,
+            age: -1,
+            bio: "New user",
+            list_of_favorites: [],
+            list_of_friends: [],
+            profile_pic: "",
+            received_friends_requests: [],
+            sent_friends_requests: [],
+            location: userLocation // Use the geolocation data
+          })
+          .then(function() {
+            console.log("New user added to firestore");
+            window.location.assign("map.html"); // Redirect to main.html after signup
+          })
+          .catch(function(error) {
+            console.log("Error adding new user: " + error);
+          });
+        }, error => {
+          console.log("Geolocation error:", error);
+          // Handle error or perform Firestore set without location
+        });
+        return false; // Prevent automatic redirect
+      } else {
+        return true; // Allow automatic redirect for existing users
+      }
     },
     uiShown: function () {
       // The widget is rendered.
@@ -17,7 +57,7 @@ var uiConfig = {
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: "popup",
-  signInSuccessUrl: "listview.html",
+  signInSuccessUrl: "main.html",
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -33,4 +73,4 @@ var uiConfig = {
   privacyPolicyUrl: "<your-privacy-policy-url>",
 };
 
-ui.start('#firebaseui-auth-container', uiConfig);
+ui.start("#firebaseui-auth-container", uiConfig);
