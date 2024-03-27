91f09319305dc66ad46858d4dfd291ea45349735
persephone-wong <persephone.w315@gmail.com>
2024-03-27 13:16:52 -0700

    Add favourite functionality to the listview

--- scripts/events.js
+++ scripts/events.js
@@ -1,3 +1,14 @@
+var currentUser
+
+function user_current() {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            currentUser = db.collection("users").doc(user.uid); //global
+            console.log(currentUser);}})
+
+}
+user_current()
+
 function displayEventCards(collection) {
     let cardTemplate = document.getElementById("eventCardTemplate");
 
@@ -8,9 +19,9 @@ function displayEventCards(collection) {
                 var title = doc.data().name;
                 var address = doc.data().address;
                 var time = doc.data().time;       
-                var date = doc.data().date;       
-                var description = doc.data().description;  // get value of the "details" key
-								var eventcode = doc.data().code;    
+                var date = doc.data().date;   
+                var docID = doc.id;
+				var eventcode = doc.data().code;    
                 let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
 
                 //update title and text and image
@@ -18,27 +29,54 @@ function displayEventCards(collection) {
                 newcard.querySelector('.card-location').innerHTML = address;
                 newcard.querySelector('.card-time').innerHTML = time;
                 newcard.querySelector('.card-date').innerHTML = date;
+                newcard.querySelector('i').id = 'save-' + docID;
+                newcard.querySelector('i').onclick = () => updateFavorite(docID);
 
                 newcard.querySelector('.card-img').src = `./images/${eventcode}.jpg`;
 
                 newcard.querySelector('.seeMoreButton').href = `/event.html?id=${doc.id}`; //link to event page
 
-                // //unique ids to all elements for future use
-                 //newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
-                 //newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
-                 //newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
-
-                //attach to gallery, Example: "hikes-go-here"
+                 currentUser.get().then(userDoc => {
+                    var favourites = userDoc.data().favorites;
+                    if (favourites.includes(docID)) {
+                       document.getElementById('save-' + docID).innerText = 'favorite';
+                    }
+              })
                 document.getElementById(collection + "-go-here").appendChild(newcard);
+
 
                 //i++;   //Optional: iterate variable to serve as unique ID
             })
         })
 }
 
+
+
+function updateFavorite(eventDocID) {
+    currentUser.get().then((userDoc)=>{
+        let favoritesNow = userDoc.data().favorites;
+        console.log(favoritesNow);
+
+        if (favoritesNow.includes(eventDocID)){
+            console.log("this event exist in the database,should be removed");
+            currentUser.update({
+            favorites: firebase.firestore.FieldValue.arrayRemove(eventDocID)
+            }).then(()=>{
+               let iconID = 'save-' + eventDocID;
+            document.getElementById(iconID).innerText = 'favorite_border';})
+        }
+        else{
+            console.log("this event does not exist in the database and should be added")
+            currentUser.update({
+                favorites: firebase.firestore.FieldValue.arrayUnion(eventDocID)
+            }).then(()=>{
+                let iconID = 'save-' + eventDocID;
+             document.getElementById(iconID).innerText = 'favorite';})
+
+        }
+
+
+
+    })}
+
 displayEventCards("events"); 
-
-
-function myFunction() {
-    document.getElementById("fav-button").innerHTML = "hello world";
-}
