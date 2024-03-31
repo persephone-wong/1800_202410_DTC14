2a98620375667c68e7c409c5d4e88c9930c2baee
OMAR_MOHAMED <mohammed.omar078@gmail.com>
2024-03-31 14:32:55 -0700

    Add html template for user cards and javascript function to read users from firestore

---
+++ scripts/displayusers.js
@@ -0,0 +1,37 @@
+db.collection("users").get().then(
+    (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(doc.data());
+      });
+    }
+  );
+  
+  // Initialize Firestore
+   
+  function displayUsers(users) {
+    
+    let cardTemplate = document.getElementById("userCardTemplate").content;
+    let usersContainer = document.getElementById(users + "-go-here");
+  
+    db.collection(users).get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          const userData = doc.data();
+  
+          let userCard = document.importNode(cardTemplate, true);
+          userCard.querySelector(".card-title").textContent = userData.name;
+          userCard.querySelector(".card-text").textContent = userData.bio;
+          userCard.querySelector(".btn-custom").dataset.uid = doc.id;
+          userCard.querySelector(".btn-custom").addEventListener("click", () => {
+            addFriend(doc.id);
+          });
+  
+          document.getElementById(users + "-go-here").appendChild(userCard);
+  
+        });
+      })
+      .catch(error => console.log(error));
+  }
+  
+  displayUsers("users");
+  
