ee190a54843faa1fbaa990906be64b85c4aa923f
persephone-wong <persephone.w315@gmail.com>
2024-04-07 15:41:55 -0700

    Add favorite functionality to the favourites page

--- scripts/favorites.js
+++ scripts/favorites.js
@@ -1,7 +1,10 @@
+var currentUser
+
 function getuser() {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
             getFavorites(user)
+            currentUser = db.collection("users").doc(user.uid);
         } else {
             console.log("No user is signed in");
         }
@@ -28,6 +31,7 @@ function getFavorites(user) {
                     var time = doc.data().time;       
                     var date = doc.data().date;   
                     var docID = doc.id;
+
 				    var eventcode = doc.data().code;    
                     let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
                     
@@ -37,18 +41,52 @@ function getFavorites(user) {
                     newcard.querySelector('.card-location').innerHTML = address;
                     newcard.querySelector('.card-time').innerHTML = time;
                     newcard.querySelector('.card-date').innerHTML = date;
-
+                    newcard.querySelector('i').id = 'save-' + docID;
+                    newcard.querySelector('i').onclick = () => updateFavorite(docID);
                     newcard.querySelector('.card-img').src = `./images/${eventcode}.jpg`;
-
                     newcard.querySelector('.seeMoreButton').href = `/event.html?id=${doc.id}`; //link to event page
 
-
+                    currentUser.get().then(userDoc => {
+                        var favourites = userDoc.data().favorites;
+                        if (favourites.includes(docID)) {
+                           document.getElementById('save-' + docID).innerText = 'favorite';
+                        }
+                  })
                     
                     document.getElementById("events-go-here").appendChild(newcard);
                 })
             });
         })
 }
+
+function updateFavorite(eventDocID) {
+    {currentUser.get().then((userDoc)=>{
+        let favoritesNow = userDoc.data().favorites;
+        console.log(favoritesNow);
+
+        if (favoritesNow.includes(eventDocID)){
+            console.log("this event exist in the database,should be removed");
+            let iconID = 'save-' + eventDocID;
+            document.getElementById(iconID).innerText = 'favorite_border';
+            currentUser.update({
+            favorites: firebase.firestore.FieldValue.arrayRemove(eventDocID)
+            }); setTimeout(() => { location.reload(); }, 500)
+        }
+        else{
+            console.log("this event does not exist in the database and should be added")
+            let iconID = 'save-' + eventDocID;
+            document.getElementById(iconID).innerText = 'favorite';
+            currentUser.update({
+            favorites: firebase.firestore.FieldValue.arrayUnion(eventDocID)
+            }); setTimeout(() => { location.reload(); }, 500)
+
+        }
+
+
+
+    })}
+}
+
 
 
 getuser();
