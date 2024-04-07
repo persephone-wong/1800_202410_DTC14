30fe274f7f6816549b94de857069bede3c42db08
OMAR_MOHAMED <mohammed.omar078@gmail.com>
2024-04-07 12:55:04 -0700

    Add friends list html template, generate from firestore, add functional searachbar

---
+++ scripts/friends_list.js
@@ -0,0 +1,46 @@
+function getuser() {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+          display_friends(user)
+        } else {
+            console.log("No user is signed in");
+        }
+    });
+  }
+  
+  
+  function display_friends(user) {
+    console.log(user)
+    db.collection("users").doc(user.uid).get()
+        .then(userDoc => {
+  
+            var friends_list = userDoc.data().list_of_friends;
+            console.log(friends_list);
+            
+            // Get pointer the new card template
+            let cardTemplate = document.getElementById("friendRequestTemplate");
+  
+            friends_list.forEach(uid => {
+              //   console.log(uid);
+                db.collection("users").doc(uid)
+                .get().then(doc => {
+                    var title = doc.data().name;      
+                    var bio = doc.data().bio;   
+                    var docID = doc.id;
+                    console.log(doc.id)
+                    var eventcode = doc.data().code;    
+                    let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+                    
+  
+                    //update title and some pertinant information
+                    newcard.querySelector('.card-title').innerHTML = title;
+                    newcard.querySelector('.card-text').innerHTML = bio;
+  
+                    
+                    document.getElementById("requests-go-here").appendChild(newcard);
+                })
+            });
+        })
+  }
+
+  getuser()
